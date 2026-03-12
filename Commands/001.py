2f97a68c534323242b9775f673f8b573a79115b0
henzogabriel954 <gabrielhenzo316@gmail.com>
2026-03-11 22:51:34 -0300

    activity 001.

---
+++ Commands/001.py
@@ -0,0 +1,9 @@
+
+# Activity 001: Write 3 Commands
+
+# 1. Make a sentence.
+print("A black coffee, please.") 
+# 2. A math problem.
+print(1.25+1.25)
+# 3. A sentence with a number.
+print("How much does it cost? \nThe black coffee costs $2.50.")
